55b8a38d5be0859c50e05f128f85cf8bf1c9d6c2
PiyapongOAK <oakpk64sakon@gmail.com>
2026-05-15 00:59:28 +0700

    Update checknow.js

--- commands/checknow.js
+++ commands/checknow.js
@@ -53,6 +53,7 @@ module.exports = {
         .setFooter({ text: 'Up-Manga อัพมังงะ • อ่านฟรีออนไลน์' })
         .setTimestamp();
 
+      await interaction.channel.send({ embeds: [embed] });
       await new Promise(r => setTimeout(r, 1500));
     }
 
